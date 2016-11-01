89a5767c498720cbe6b22e60b6ad2235eefa3dc5
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-11-01 15:50:20 -0500

    filtrando gastos terminado

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,15 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+
+ 	if params[:concept].present? && params[:category_id].present? 
+ 		@expenses=Expense.where("concept like ? and category_id = ?", "%#{params[:concept]}%",  "#{params[:category_id]}")
+  	elsif params[:concept].present?
+  		@expenses=Expense.where("concept like ?","%#{params[:concept]}%")
+  	elsif params[:category_id].present? 
+  		@expenses=Expense.where("category_id = ?","#{params[:category_id]}")
+  	else	
+  		@expenses = Expense.order("date DESC")
+  	end
+     
   end
 end
