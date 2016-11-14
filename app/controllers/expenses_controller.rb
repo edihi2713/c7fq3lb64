49ec0a871cca41fcc7ea108af2e12028b28ca344
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-11-13 22:59:36 -0500

    version3

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,21 +1,19 @@
 class ExpensesController < ApplicationController
 	before_action :authenticate_user!
 
-  def index
- 	if params[:concept].present? && params[:category_id].present? 
- 		@expenses=Expense.where("concept like ? and category_id = ?", "%#{params[:concept]}%",  "#{params[:category_id]}")
-  	elsif params[:concept].present?
-  		@expenses=Expense.where("concept like ?","%#{params[:concept]}%")
-  	elsif params[:category_id].present? 
-      
-  		@expenses=Expense.where("category_id = ?","#{params[:category_id]}")
-  	else	
-  		@expenses = Expense.order("date DESC")
-  	end
-    @user = User.find(current_user.id)
-    @expenses = @user.expenses
+def index
 
-  end
+    @user = User.find(current_user.id)
+    @expenses = @user.expenses.order("date DESC")
+
+    if params[:concept].present?
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+    end
+    if params[:category_id].present?
+      @expenses = @expenses.where("category_id = ?", params[:category_id])
+    end
+
+end
 
   
 end
